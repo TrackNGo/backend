1037b02d9a927e949820efea0e1d35deecfbc9d7
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-12-04 18:19:06 +0530

    fix: fix error

--- src/routes/busLocationRoutes.js
+++ src/routes/busLocationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const { updateBusLocatioin } = require('../controller/busController');
+const { updateBusLocatioin } = require('../controller/busLocationController');
 
 // API to update bus location
 route.put('/update-location', updateBusLocatioin);
