ed42f81c89c7e35a298c36411b2cddf8676cf91d
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-12-04 16:44:13 +0530

    feat: client routes

---
+++ src/routes/user.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const route = express.Router();
+const { getRoute, getBusLocation, getLocationCodeSearchByName, getBus} = require('../controller/userController');
+
+// API to fetch all bus locations
+route.post('/searchFor-buses', getBus);
+
+// API to fetch all bus locations
+route.post('/getSpecific-busRoute', getRoute);
+
+// API to fetch all bus locations
+route.get('/getBus-locations/:busId', getBusLocation);
+
+// Get location code by serching 
+route.post('/getLocationCode-searchByName', getLocationCodeSearchByName);
+
+// Get route paths 
+//route.post('/get-route', all_function.get_route);
+
+module.exports = route;
