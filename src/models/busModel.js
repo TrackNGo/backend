a7610152210bcfadd964d098c9a0ab3442359fd6
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-01-02 17:29:00 +0530

    feat: update bus model

---
+++ src/models/busModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const busModelSchema = new mongoose.Schema(
+    {
+        busNumber: {
+            type: String,
+            required: true
+        },
+        startLocation: {
+            type: String,
+            required: true
+        },
+        endLocation: {
+            type: String,
+            required: true
+        },
+        routeNumber: {
+            type: String,
+            required: true
+        },
+        fareEstimate: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            enum: ['Normal', 'Semi-Luxury', 'Luxury'], default: 'Normal'
+        },
+        status: {
+            type: Boolean,
+            default: false
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+const BusModel = mongoose.model('Bus', busModelSchema);
+
+module.exports = BusModel;
+
