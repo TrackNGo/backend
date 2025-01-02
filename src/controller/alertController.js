e2585e43d33934cb3fe572cd6460d60155413af9
RuwandiPerera <139639701+RuwandiPerera@users.noreply.github.com>
2025-01-02 19:47:51 +0530

    add some changes to alert model

---
+++ src/controller/alertController.js
@@ -0,0 +1,20 @@
+const BusRoute = require('../models/busRouteModel');
+const BusLocation = require('../models/busLocationModel');
+const axios = require('axios')
+
+async function setAlert(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+
+
+module.exports = {
+    
+}
--- src/models/alert.js
+++ src/models/alert.js
@@ -10,7 +10,7 @@ const Alert = new mongoose.Schema({
         type : String,
         required: true
     },
-    dicription: {
+    decription: {
         type : String,
         required: true
     },
