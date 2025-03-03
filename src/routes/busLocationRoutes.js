5e34d20bbbfa00266b5f5c576e5cfeae367e255d
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-03-03 19:49:30 +0530

    feat: start end  location tracking system-conductor

--- src/routes/busLocationRoutes.js
+++ src/routes/busLocationRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const route = express.Router();
-const { updateBusLocation, getDistance } = require('../controller/busLocationController');
+const { getDistance, updateBusLocation } = require('../controller/busLocationController')
+const { setBusStatus, checkBusStatus } = require('../controller/conductorController')
+const { setAlert } = require('../controller/alertController')
 
 // API to update bus location
 route.put('/update-location', updateBusLocation);
 route.post('/get-location-distance', getDistance);
+route.put('/update-bus-status', setBusStatus);
+route.get('/get-bus-status/:busNumber', checkBusStatus);
+route.post('/set-bus-alert', setAlert)
 
 module.exports = route;
