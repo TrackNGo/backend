edda4ca5d65fba318f6bbddbc6bc316a902124fa
Udara Nalawansa <udarakavishka13@gmail.com>
2025-03-30 04:10:29 +0530

    feat: add GET endpoint to retrieve all bus service requests

--- src/routes/contactRoute.js
+++ src/routes/contactRoute.js
@@ -3,6 +3,7 @@ const contactController = require('../controller/contactController');
 const router = express.Router();
 
 router.post('/bus-service', contactController.submitBusService);
+router.get('/bus-service', contactController.getAllBusServiceRequests);
 router.post('/technical', contactController.submitTechnicalRequest);
 router.get('/', contactController.getAllContacts);
 
